d80ed1a8dbfcf04d96dcef7f367e7a575983af62
Jonas Natten <jonas@natten.it>
2024-08-29 16:08:34 +0200

    vim: Fix indent-blankline config after update

--- neovim/.config/nvim/lua/indent-blankline-cfg.lua
+++ neovim/.config/nvim/lua/indent-blankline-cfg.lua
@@ -2,35 +2,37 @@ vim.opt.list = true
 -- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 
+require("ibl").setup()
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    context_char = "▎",
-    show_current_context_start = true,
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
-    use_treesitter = true,
-    show_current_context = true,
-    context_patterns = {
-    	"class",
-    	"return",
-    	"function",
-    	"method",
-    	"^if",
-    	"^while",
-    	"jsx_element",
-    	"^for",
-    	"^object",
-    	"^table",
-    	"block",
-    	"arguments",
-    	"if_statement",
-    	"else_clause",
-    	"jsx_element",
-    	"jsx_self_closing_element",
-    	"try_statement",
-    	"catch_clause",
-    	"import_statement",
-    	"operation_type",
-    }
-}
+-- Version 2 stuff, can probably be deleted yolo
+-- require("indent_blankline").setup {
+--     space_char_blankline = " ",
+--     context_char = "▎",
+--     show_current_context_start = true,
+--     show_trailing_blankline_indent = false,
+--     show_first_indent_level = true,
+--     use_treesitter = true,
+--     show_current_context = true,
+--     context_patterns = {
+--     	"class",
+--     	"return",
+--     	"function",
+--     	"method",
+--     	"^if",
+--     	"^while",
+--     	"jsx_element",
+--     	"^for",
+--     	"^object",
+--     	"^table",
+--     	"block",
+--     	"arguments",
+--     	"if_statement",
+--     	"else_clause",
+--     	"jsx_element",
+--     	"jsx_self_closing_element",
+--     	"try_statement",
+--     	"catch_clause",
+--     	"import_statement",
+--     	"operation_type",
+--     }
+-- }
