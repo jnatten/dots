47aa26226f425421be5ebdac7b49a6566372796e
Jonas Natten <jonas@natten.it>
2024-09-16 10:19:40 +0200

    vim: Comment out some treesitter cfg that was broken

--- neovim/.config/nvim/lua/treesitter-cfg.lua
+++ neovim/.config/nvim/lua/treesitter-cfg.lua
@@ -1,5 +1,5 @@
-require("nvim-treesitter.configs").setup {
-	indent = { enable = true },
-	highlight = { enable = true },
-	matchup = { enable = true }
-}
+-- require("nvim-treesitter.configs").setup {
+-- 	indent = { enable = true },
+-- 	highlight = { enable = true },
+-- 	matchup = { enable = true }
+-- }
