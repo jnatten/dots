95451de3926aaa3df8e0cdd47fb78deacf8cab7d
Jonas Natten <jonas@natten.it>
2024-12-11 21:35:05 +0100

    neovim: Add treesitter

---
+++ neovim/.config/nvim/lua/config/settings.lua
@@ -0,0 +1,24 @@
+local global = vim.g
+local o = vim.opt
+
+o.number = true
+o.clipboard = "unnamedplus"
+o.syntax = "on"
+o.autoindent = true
+o.cursorline = true
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.encoding = "UTF-8"
+o.ruler = true
+o.mouse = "a"
+o.title = true
+o.hidden = true
+o.ttimeoutlen = 0
+o.wildmenu = true
+o.showcmd = true
+o.showmatch = true
+o.inccommand = "split" -- When nonempty, shows the effects of :substitute, :smagic, :snomagic and user commands with the :command-preview flag as you type.
+o.splitright = true
+o.splitbelow = true
+o.termguicolors = true
