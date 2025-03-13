c0f32fffab88150118d9deceba6e8bcf4cf682f5
Jonas Natten <jonas@natten.it>
2025-03-13 13:22:11 +0100

    neovim: Blink binds

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -3,7 +3,14 @@ return {
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "*",
 	opts = {
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "default",
+			["<CR>"] = { "accept", "fallback" },
+			["("] = { "accept", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+		},
+		completion = { accept = { auto_brackets = { enabled = true } } },
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
