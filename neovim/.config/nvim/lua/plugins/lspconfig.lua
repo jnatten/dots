5a91560017e894ff55e387ec6d31b41e8fd86994
Jonas Natten <jonas@natten.it>
2025-03-13 13:00:45 +0100

    neovim: add blink.nvim

--- neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,110 +1,46 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "folke/neodev.nvim", opts = {} },
+	dependencies = { "saghen/blink.cmp" },
+	opts = {
+		servers = {
+			ts_ls = {},
+			cssls = {},
+			tailwindcss = {},
+			html = {},
+			jsonls = {},
+			terraformls = {},
+			eslint = {},
+			pyright = {},
+			vtsls = {
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+					"vue",
+				},
+				settings = {
+					typescript = {
+						preferences = {
+							importModuleSpecifier = "relative",
+						},
+					},
+					javascript = {
+						preferences = {
+							importModuleSpecifier = "relative",
+						},
+					},
+				},
+			},
+		},
 	},
-	config = function()
-		local nvim_lsp = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
-
-		local protocol = require("vim.lsp.protocol")
-
-		local on_attach = function(client, bufnr)
-			-- format on save
-			if client.server_capabilities.documentFormattingProvider then
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = vim.api.nvim_create_augroup("Format", { clear = true }),
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format()
-					end,
-				})
-			end
+	config = function(_, opts)
+		local lspconfig = require("lspconfig")
+		for server, config in pairs(opts.servers) do
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			lspconfig[server].setup(config)
 		end
-
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-		mason_lspconfig.setup_handlers({
-			function(server)
-				nvim_lsp[server].setup({
-					capabilities = capabilities,
-				})
-			end,
-			-- ["ts_ls"] = function()
-			-- 	nvim_lsp["ts_ls"].setup({
-			-- 		on_attach = on_attach,
-			-- 		capabilities = capabilities,
-			-- 	})
-			-- end,
-			["cssls"] = function()
-				nvim_lsp["cssls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["tailwindcss"] = function()
-				nvim_lsp["tailwindcss"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["html"] = function()
-				nvim_lsp["html"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["jsonls"] = function()
-				nvim_lsp["jsonls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["terraformls"] = function()
-				nvim_lsp["terraformls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["eslint"] = function()
-				nvim_lsp["eslint"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["pyright"] = function()
-				nvim_lsp["pyright"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["vtsls"] = function()
-				nvim_lsp["vtsls"].setup({
-					filetypes = {
-						"javascript",
-						"javascriptreact",
-						"javascript.jsx",
-						"typescript",
-						"typescriptreact",
-						"typescript.tsx",
-						"vue",
-					},
-					settings = {
-						typescript = {
-							preferences = {
-								importModuleSpecifier = "relative",
-							},
-						},
-						javascript = {
-							preferences = {
-								importModuleSpecifier = "relative",
-							},
-						},
-					},
-				})
-			end,
-		})
 	end,
 }
