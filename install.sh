c9c7d3273542dbf78c6f6f0f3e8d0d30836ab41d
Jonas Natten <jonas@natten.it>
2025-06-06 12:17:35 +0200

    make install script interactive

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-echo "cd to $GIT_ROOT\n"
 cd $GIT_ROOT
 
-for pkg in `exa -D`; do
-  echo "Installing $pkg"
-  stow $pkg --target $HOME
+for pkg in `eza -D`; do
+  echo "Do you wish to install this $pkg?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes ) echo "Installing $pkg..."; stow $pkg --target $HOME && echo "$pkg installed..."; break;;
+          No ) echo "Skipping $pkg"; break;;
+      esac
+  done
 done
