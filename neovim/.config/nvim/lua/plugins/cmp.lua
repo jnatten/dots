4f2d0ab1ec4429fc0e0b25d5cd30f9f028af95f2
Jonas Natten <jonas@natten.it>
2025-08-07 09:33:21 +0200

    neovim: add documentation to blink.nvim and bindings

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -6,13 +6,33 @@ return {
 		keymap = {
 			preset = "default",
 			["<CR>"] = { "accept", "fallback" },
+			["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
 			["("] = { "accept", "fallback" },
 			["<C-k>"] = { "select_prev", "fallback" },
 			["<C-j>"] = { "select_next", "fallback" },
 		},
-		completion = { accept = { auto_brackets = { enabled = true } } },
+		cmdline = {
+			keymap = {
+				["<C-k>"] = { "show", "select_prev", "fallback" },
+				["<C-j>"] = { "show", "select_next", "fallback" },
+			},
+		},
+		completion = {
+			accept = {
+				auto_brackets = { enabled = true },
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 100,
+				treesitter_highlighting = true,
+				window = {
+					max_width = 100,
+					border = "rounded",
+				},
+			},
+		},
 		appearance = {
-			use_nvim_cmp_as_default = true,
+			use_nvim_cmp_as_default = false,
 			nerd_font_variant = "mono",
 		},
 		signature = {
