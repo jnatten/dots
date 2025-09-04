ff80ea74ca86322730ff650b7a46178c01da5f7b
Jonas Natten <jonas@natten.it>
2025-09-04 14:44:12 +0200

    tmux: make capture pane open in neovim

--- tmux/.tmux/capture-and-edit.sh
+++ tmux/.tmux/capture-and-edit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Capture the current tmux pane and open it in $EDITOR in a new tmux window
 tmpfile="/tmp/tmux-output-$(date +%s).txt"
-tmux capture-pane -pS - | awk 'NF {line=$0; print line}' > "$tmpfile"
-tmux new-window -n "Pane Capture" "$EDITOR \"$tmpfile\""
+tmux capture-pane -p -e -S - | awk 'NF {line=$0; print line}' > "$tmpfile"
+tmux new-window -n "Pane Capture" "nvim -c 'terminal cat \"$tmpfile\"' -c 'normal G'"
