98115bc22b7760490ee34e8f6dd610db4db30d61
Jonas Natten <jonas@natten.it>
2026-04-29 12:50:40 +0200

    fish: ctrl+f to accept autosuggestion

--- fish/.config/fish/bindings.fish
+++ fish/.config/fish/bindings.fish
@@ -17,3 +17,6 @@ bind -M insert \cp up-or-search
 # Ctrl-N to previous command
 bind \cp down-or-search
 bind -M insert \cp down-or-search
+
+bind \cf accept-autosuggestion
+bind -M insert \cf accept-autosuggestion
