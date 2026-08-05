36af5bd4db51a598922678ebab86f223bfcf570e
Jonas Natten <jonas@natten.it>
2026-08-05 09:13:50 +0200

    neovim: diffview options

--- neovim/.config/nvim/lua/plugins/diffview.lua
+++ neovim/.config/nvim/lua/plugins/diffview.lua
@@ -7,10 +7,20 @@ return {
 		"DiffviewDiffDirs",
 	},
 	opts = {
+		auto_close_on_empty = true,
+		clean_up_buffers = true,
+		enhanced_diff_hl = true,
+		hide_merge_artifacts = true,
+		show_help_hints = false,
+		use_icons = true,
 		view = {
 			merge_tool = {
 				layout = "diff4_mixed",
+				disable_diagnostics = true,
 				winbar_info = true,
+			},
+			cycle_layouts = {
+				merge_tool = { "diff4_mixed", "diff3_mixed", "diff3_horizontal", "diff1_plain" },
 			},
 		},
 	},
